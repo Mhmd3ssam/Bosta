d4cfaf6eb7594071faadf6b0c6b3d10af6223798
MohamedEssam1695 <toma126542@gmail.com>
2022-01-27 20:27:58 +0200

    implement service functions to serv details comp

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './style.css'
+import { generateDate, generateTime, translateState } from '../../Service';
+
 export default function Details({ data }) {
     console.log(data);
     return (
@@ -13,12 +15,17 @@ export default function Details({ data }) {
                 </tr>
             </thead>
             <tbody>
-                {data?.TransitEvents.map(({state,timestamp}) => {
+                {data?.TransitEvents.map(({state,timestamp,reason}) => {
                     return (
                         <tr>
-                            <td >{state}</td>
-                            <td>{timestamp.split('T')[1].split(".")[0]}</td>
-                            <td>{timestamp.split('T')[0]}</td>
+                            <td >
+                                <>
+                                    <p>{translateState(state)}</p>
+                                    {reason?reason:null}
+                                </>
+                             </td>
+                            <td>{generateTime(timestamp)}</td>
+                            <td>{generateDate(timestamp)}</td>
                             <td></td>
                         </tr>
                     )
---
+++ src/Service/index.js
@@ -0,0 +1,37 @@
+
+export function generateDate(timestamp){
+    return timestamp.split('T')[0];
+}
+
+export function generateTime(timestamp){
+    let fullTime = timestamp.split('T')[1];
+    let indx = fullTime.lastIndexOf(':');
+    let charAt = fullTime.charAt(indx);
+    let hours = fullTime.split(charAt)[0];
+    let ments = fullTime.split(charAt)[1];
+    return `${hours}:${ments}`;
+}
+
+export function translateState(state){
+    switch(state){
+        case 'TICKET_CREATED':
+            return 'تم انشاء الشحنة'
+        case 'PACKAGE_RECEIVED':
+            return 'تم استلام الشحنه من التاجر'
+        case 'IN_TRANSIT':
+            return 'في مرحلة انتقالية'
+        case 'NOT_YET_SHIPPED':
+            return 'لم يتم شحنه بعد'  
+        case 'OUT_FOR_DELIVERY':
+            return 'الشحنة خرجت للتسليم'
+        case 'DELIVERED':
+            return 'تم التسليم'  
+        case 'RECEIVED_DELIVERY_LOCATION':
+            return 'تم استلام موقع تسليم الشحنة'  
+        case 'WAITING_FOR_CUSTOMER_ACTION':
+            return 'في انتظار رد العميل' 
+        case 'DELIVERED_TO_SENDER':
+            return 'تم تسليم الشحنة للمسلم اليه'           
+
+    }
+}
