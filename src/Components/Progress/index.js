c89c827331d753bcb2e6c550f8c85768372811fa
MohamedEssam1695 <toma126542@gmail.com>
2022-01-30 02:50:47 +0200

    fix progress

--- src/Components/Progress/index.js
+++ src/Components/Progress/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./style.css";
 import { getRecevingDate, getLastUpdate, getLastState } from '../../Service';
-import approval from '../../assets/approval.png';
 import van from '../../assets/van.png';
 import delivered from '../../assets/delivered.png';
 import correct from '../../assets/correct.png';
@@ -9,7 +8,7 @@ import check from "../../assets/check.png";
 
 export default function Progress({ data }) {
     const [currentState, setCurrentState] = useState(data?.CurrentStatus.state);
-
+    console.log(data?.CurrentStatus.state)
     return (
         <>
             <div className="detailContainer">
@@ -33,7 +32,7 @@ export default function Progress({ data }) {
                     <p className='title '>{`${data?.TrackingNumber} رقم الشحنة`}</p>
                     <p className="titleValue"
                         style={{
-                            color:currentState === "DELIVERED"?"#4cac4c":"#ffd200"
+                            color:data?.CurrentStatus.state === "DELIVERED"?"#4cac4c":"#ffd200"
                         }}
                     >
                         {getLastState(data?.CurrentStatus.state)}
@@ -45,11 +44,11 @@ export default function Progress({ data }) {
                     <div className="progressBar"
                         style={{
                             backgroundColor: 
-                            currentState === "DELIVERED" ? "#4cac4c":""
+                            data?.CurrentStatus.state === "DELIVERED" ? "#4cac4c":""
                         }}
                     >
                         <div className="leftIconContainer" >
-                            <img src={currentState === "DELIVERED" ? correct : delivered}
+                            <img src={data?.CurrentStatus.state === "DELIVERED" ? correct : delivered}
                                 style={{ backgroundColor: "transparent" }
                                 }
                             />
@@ -60,13 +59,13 @@ export default function Progress({ data }) {
                     <div className="progressBar"
                         style={{
                             backgroundColor: 
-                            currentState === "DELIVERED" ? "#4cac4c":
-                            currentState === "OUT_FOR_DELIVERY" ?"#ffd200":""
+                            data?.CurrentStatus.state === "DELIVERED" ? "#4cac4c":
+                            data?.CurrentStatus.state === "OUT_FOR_DELIVERY" ?"#ffd200":""
                          }}
                     >
                         <div className="middleIconContainer" >
-                            <img src={currentState === "DELIVERED" ? correct
-                                : currentState === "OUT_FOR_DELIVERY" ? check : van
+                            <img src={data?.CurrentStatus.state === "DELIVERED" ? correct
+                                : data?.CurrentStatus.state === "OUT_FOR_DELIVERY" ? check : van
                             }
                                 style={{ backgroundColor: "transparent" }
                                 }
@@ -78,14 +77,16 @@ export default function Progress({ data }) {
                     <div className="progressBar"
                         style={{
                            backgroundColor: 
-                           currentState === "DELIVERED" ? "#4cac4c":
-                           currentState === "TICKET_CREATED" ?"":"#ffd200"
+                           data?.CurrentStatus.state === "DELIVERED" ? "#4cac4c":
+                           data?.CurrentStatus.state === "TICKET_CREATED" ?"":
+                           data?.CurrentStatus.state === ""?"":"#ffd200"
                         }}
                     >
                         <div className="middleIconContainer" >
                             <img src={
-                                currentState === "DELIVERED" ? correct
-                                    : currentState === "TICKET_CREATED" ? van : check
+                                data?.CurrentStatus.state === "DELIVERED" ? correct
+                                    : data?.CurrentStatus.state === "TICKET_CREATED" ? van :
+                                    data?.CurrentStatus.state === "" ? van: check
                             }
                                 style={{ backgroundColor: "transparent" }
                                 }
@@ -94,7 +95,7 @@ export default function Progress({ data }) {
                         <div className="rightIconContainer">
                             <img
                                 src={
-                                    currentState === "DELIVERED" ? correct : check
+                                    data?.CurrentStatus.state === "DELIVERED" ? correct : check
                                 }
                                 style={{ backgroundColor: "transparent" }} />
                         </div>
@@ -118,7 +119,7 @@ export default function Progress({ data }) {
         </>
     );
 }
-// "TICKET_CREATED" "PACKAGE_RECEIVED" "OUT_FOR_DELIVERY" "DELIVERED" "CANCELED"
+// "TICKET_CREATED" "PACKAGE_RECEIVED" "OUT_FOR_DELIVERY" "DELIVERED" "CANCELED" DELIVERED 
 // green #4cac4c yellow #ffd200
 
 //style={{
