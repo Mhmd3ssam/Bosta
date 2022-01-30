6f512c7c4278efbc96bdaaa13d70610868cd5073
MohamedEssam1695 <toma126542@gmail.com>
2022-01-30 18:23:52 +0200

    handel erreor message

--- src/Components/ErrorBoundary/index.js
+++ src/Components/ErrorBoundary/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import "./style.css"
 import Home from '../../Pages/trackingService';
 
 
 export default class ErrorBoundary extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { hasError: false };
-    }
-  
-    static getDerivedStateFromError(error) {
-      return { hasError: true };
-    }
-  
-    render() {
-      if (this.state.hasError) {
-        return <Home/>
-      }
-  
-      return this.props.children; 
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
   }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <>
+          <Home />
+          <div className="messageContainer">
+            <h3 >
+              {' الرجاء إدخال رقم الشحنة بشكل صحيح وتتبع الشحنة الخاصة بك '}
+            </h3>
+          </div>
+        </>)
+
+    }
+
+    return this.props.children;
+  }
+}
